aef683bcc757488655d8175df9dfa70330a6e3ee
jarrenjames13 <jarrenjames13@gmail.com>
2025-03-12 16:52:43 +0800

    restarting cart and order modal

--- controllers/mldi/inventory-controller.js
+++ controllers/mldi/inventory-controller.js
@@ -1,27 +1,27 @@
-// import { getInventory, getPrices } from "../../models/mldi/inventory.js";
+import { getInventory, getPrices } from "../../models/mldi/inventory.js";
 
-// export const getInventory_Cont = async (req, res) => {
-//     try {
-//         const { company } = req.query;
+export const getInventory_Cont = async (req, res) => {
+    try {
+        const { company } = req.query;
 
-//         const inventory = await getInventory(company);
+        const inventory = await getInventory(company);
 
-//         res.json(inventory);
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
+        res.json(inventory);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+};
 
-// export const getPrices_Cont = async (req, res) => {
-//     try {
-//         const { invt_id } = req.query
+export const getPrices_Cont = async (req, res) => {
+    try {
+        const { invt_id } = req.query
 
-//         const prices = await getPrices(invt_id)
+        const prices = await getPrices(invt_id)
 
-//         res.json(prices)
-//     } catch (err) {
-//         console.log(err)
-//         throw err
-//     }
-// };
+        res.json(prices)
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+};
--- utils/mldi/dbcreds.js
+++ utils/mldi/dbcreds.js
@@ -1,5 +1,5 @@
 
-// export const database = process.env.SOLOMON_DB
-// export const user = process.env.SOLOMON_DB_USER
-// export const password = process.env.SOLOMON_DB_PASSWORD
-// export const server = process.env.SOLOMON_DB_SERVER
+export const database = process.env.SOLOMON_DB
+export const user = process.env.SOLOMON_DB_USER
+export const password = process.env.SOLOMON_DB_PASSWORD
+export const server = process.env.SOLOMON_DB_SERVER
